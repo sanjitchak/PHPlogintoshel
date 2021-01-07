27ca364c4afb6bef52527e1f347feaf338af10c7
Sanjit <sanjit@sanjitchakrabarti.com>
2021-01-07 19:16:13 +0530

    script changes to run command

--- done.php
+++ done.php
@@ -2,7 +2,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Too fast!</title>
+    <title>Too fast! Restore may not have happened. Contact Sanjit at https://growonlinetoday.com/calendar</title>
 </head>
 <body>
 Too fast!
